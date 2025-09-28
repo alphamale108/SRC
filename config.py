286e3ab1b4a44cac620a7f0d3fd55bbf76536a07
alphamale108 <136615536+alphamale108@users.noreply.github.com>
2025-09-28 23:52:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ except:
 
 # Pyrogram setup
 class PyroConf(object):
-    API_ID = int(getenv("API_ID", "6"))
-    API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    API_ID = int(getenv("API_ID", ""))
+    API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN")
     SESSION_STRING = getenv("SESSION_STRING")
     BOT_START_TIME = time()
